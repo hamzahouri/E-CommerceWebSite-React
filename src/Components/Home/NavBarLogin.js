dd257502d011d89419837e3a8f78307f63dd69a1
Hamza Houri <Hamza.HOURI-ext@um6p.ma>
2024-01-04 17:22:51 +0100

    project initialisation

---
+++ src/Components/Home/NavBarLogin.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavBarLogin = () => {
+  return (
+    <div>NavBarLogin</div>
+  )
+}
+
+export default NavBarLogin
---
+++ src/Pages/Home/HomePage.js
@@ -0,0 +1,17 @@
+import react from 'react'
+import NavBarLogin from '../../Components/Home/NavBarLogin';
+import Slider from '../../Components/Home/Slider';
+
+const HomePage = () => {
+
+    return (
+        <div>
+            <NavBarLogin/>
+            <Slider/>
+الرئيسية
+        </div>
+    )
+}
+
+
+export default HomePage;
