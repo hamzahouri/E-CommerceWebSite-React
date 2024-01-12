75829f817c8d7500ff32b449cee84a96ebcb9d93
Hamza Houri <Hamza.HOURI-ext@um6p.ma>
2024-01-12 12:08:29 +0100

    add register and login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './Pages/Home/HomePage';
+import NavBarLogin from './Components/Utility/NavBarLogin';
+import Footer from './Components/Utility/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginPage from './Pages/Auth/LoginPage';
+import RegisterPage from './Pages/Auth/RegisterPage';
 
 function App() {
   return (
-    <div className='text-center'>     
-      <HomePage/>
+    <div className='font'>  
+     <NavBarLogin/>
+    <BrowserRouter> 
+    <Routes>
+      <Route index element={<HomePage/> }  />
+      <Route path='/login' element={<LoginPage/> }  />     
+      <Route path='/register' element={<RegisterPage/> }  /> 
+
+      </Routes>
+      </BrowserRouter>  
+      <Footer/>
     </div>
   );
 }
